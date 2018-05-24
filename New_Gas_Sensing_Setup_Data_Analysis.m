e210e526a8b25c42df0588c8012e1c5b877ee21a
Sean Hwang <14366381+KingBaba@users.noreply.github.com>
2018-05-24 13:04:10 -0400

    Work in Progress

--- New_Gas_Sensing_Setup_Data_Analysis.m
+++ New_Gas_Sensing_Setup_Data_Analysis.m
@@ -1,6 +1,6 @@
 %Type out file directory
 
-Directory = 'C:\Users\Sean\Box Sync\Graduate School\Research\Data\Sensor\New Gas Sensing\Analysis';
+Directory = 'C:\Users\seani\Box Sync\Graduate School\Research\Data\Sensor\New Gas Sensing\Analysis';
 cd(Directory);
 %Fill out information below.
 
@@ -8,6 +8,15 @@ Material = 'P3-TiO2';
 Media = 'Air';
 Analyte = 'Acetone';
 Chip_ID = 'P3-TiO2-1, P3-TiO2-2';
+Working_Devices = 21;
+
+%Experimental Parameters
+
+DAQ_Interval = 1; %Time delay between each measurement cycle in seconds.
+Exposure_Time = 5; %Duration of the analyte exposure
+Purge_Time = 10; %Duration of the purge
+Exposure_Time_Points = [60; 75; 90; 105; 120; 135; 150; 165; 180; 195; 210; 225; 240;];
+Exposure_Concentrations = [1; 1; 1; 2.5; 5; 7.5; 10; 100; 100; 100; 250; 400; 573; 90];
 
 %Include any additional experimental details.
 
@@ -22,11 +31,13 @@ Output_File_Name = 'Analysis iT';
 %devices. The start and end rows indicate the range of rows that correspond 
 %to 1 IVg plot measurement.
 
-Experimental_Data = importdata('2018-05-11 - P3-TiO2 Pd Pt Iso-sol Purus Nano Acetone and Humidity Sensing 2 Analysis.xlsx');
+Raw_Experimental_Data = importdata('2018-05-11 - P3-TiO2 Pd Pt Iso-sol Purus Nano Acetone and Humidity Sensing 2 Analysis.xlsx');
+Organized_Experimental_Data = Raw_Experimental_Data.data(:,17:17 + Working_Devices);
 Data_Points = size(Experimental_Data.data,1);
 
 x_time = Experimental_Data.data(:,1);
 x_linspace = linspace(1,Data_Points,Data_Points);
+
 A = Experimental_Data.data(:,2);
 B = Experimental_Data.data(:,3);
 C = Experimental_Data.data(:,3);
@@ -38,19 +49,19 @@ Response_End = 36235;
 Recovery_Start = 36376;
 Recovery_End = 37800;
 
-A_Response_1 = Experimental_Data.data(Response_Start:Response_End,2);
-A_Response_Change = Experimental_Data.data(Response_Start:Response_End,2)-Experimental_Data.data(Response_Start,2);
-A_Response_Time = (Experimental_Data.data(Response_Start:Response_End,1)-Experimental_Data.data(Response_Start,1))/1000;
+%A_Response_1 = Experimental_Data.data(Response_Start:Response_End,2);
+%A_Response_Change = Experimental_Data.data(Response_Start:Response_End,2)-Experimental_Data.data(Response_Start,2);
+%A_Response_Time = (Experimental_Data.data(Response_Start:Response_End,1)-Experimental_Data.data(Response_Start,1))/1000;
 
-A_Recovery_1 = Experimental_Data.data(Recovery_Start:Recovery_End,2);
-A_Recovery_Change = Experimental_Data.data(Recovery_Start:Recovery_End,2)-Experimental_Data.data(Response_Start,2);
-A_Recovery_Time = (Experimental_Data.data(Recovery_Start:Recovery_End,1)-Experimental_Data.data(Recovery_Start,1))/1000+1;
+%A_Recovery_1 = Experimental_Data.data(Recovery_Start:Recovery_End,2);
+%A_Recovery_Change = Experimental_Data.data(Recovery_Start:Recovery_End,2)-Experimental_Data.data(Response_Start,2);
+%A_Recovery_Time = (Experimental_Data.data(Recovery_Start:Recovery_End,1)-Experimental_Data.data(Recovery_Start,1))/1000+1;
 
-figure
-plot(A)
+%figure
+%plot(A)
 
-figure
-plot(A_Response_Change)
+%figure
+%plot(A_Response_Change)
 
-figure
-plot(A_Recovery_Change)
+%figure
+%plot(A_Recovery_Change)
