a7654a505154183ae4d2746817588f4bbc9c3ae6
Sean Hwang <>
2018-05-30 17:05:01 -0400

    Work in progress

--- New_Gas_Sensing_Setup_Data_Analysis.m
+++ New_Gas_Sensing_Setup_Data_Analysis.m
@@ -12,7 +12,7 @@ Chip_ID = {'TiO2-1', 'TiO2-2'};
 %Experimental Parameters
 
 DAQ_Interval = 1; %Time delay between each measurement cycle in seconds.
-Background_Collection_Period = 5; %Time before MFC starts
+Background_Collection_Period = 5 + 3/60; %Time before MFC starts
 Exposure_Period = 5; %Duration of the analyte exposure in minutes
 Purge_Period = 10; %Duration of the purge in minutes
 Exposure_Time_Points = [60; 75; 90; 105; 120; 135; 150; 165; 180; 195; 210; 225; 240]; %
@@ -45,14 +45,14 @@ Raw_SourceMeter_Data =  importdata('2018-05-10 - P3-TiO2 Acetone and Humidity Se
 Chip_Count = size(Chip_ID, 2);
 Devices_Count = size(Raw_SourceMeter_Data.data,2)-1;
 
-Sensing_Data.Device_ID = Raw_SourceMeter_Data.textdata(1,:);
+NSensing_Data.Device_ID = Raw_SourceMeter_Data.textdata(1,:);
 Sensing_Data.Full_Data = Raw_SourceMeter_Data.data;
 Sensing_Data.Concentrations = Exposure_Concentrations;
 
 for count1 = 1:Exposures
     
     Field_Variable = compose("Exposure%d", count1);
-    Exposure_Start = fix((Exposure_Time_Points(count1)+5)*60/DAQ_Interval+3);
+    Exposure_Start = fix((Exposure_Time_Points(count1)+Background_Collection_Period)*60/DAQ_Interval+3);
     Purge_End = fix(Exposure_Start + (Exposure_Period + Purge_Period)*60/DAQ_Interval);
     Sensing_Data.(Field_Variable) = Sensing_Data.Full_Data(Exposure_Start:Purge_End,:);
     Sensing_Data.(Field_Variable)(:,1) = Sensing_Data.(Field_Variable)(:,1)-Sensing_Data.(Field_Variable)(1,1);
@@ -72,13 +72,28 @@ end
 %A_Recovery_Change = Experimental_Data.data(Recovery_Start:Recovery_End,2)-Experimental_Data.data(Response_Start,2);
 %A_Recovery_Time = (Experimental_Data.data(Recovery_Start:Recovery_End,1)-Experimental_Data.data(Recovery_Start,1))/1000+1;
 
-X = Sensing_Data.Exposure1(:,1);
-Y = Sensing_Data.Exposure1(:,2);
-X1 = X(1:100);
-Y1 = Y(1:100);
+X = Sensing_Data.Exposure7(:,1);
+Y = (Sensing_Data.Exposure7(:,5)-Sensing_Data.Exposure7(1,5))/Sensing_Data.Exposure7(1,5);
+X1 = X(1:300);
+Y1 = Y(1:300);
+
+X2 = X(301:901);
+Y2 = Y(301:901);
+
+X3 = Sensing_Data.Full_Data(305:605,1)-Sensing_Data.Full_Data(305,1);
+Y3 = (Sensing_Data.Full_Data(305:605,5)-Sensing_Data.Full_Data(305,5))/Sensing_Data.Full_Data(305,5);
+
+figure
+plot(X3,Y3)
+
+figure
+plot(Sensing_Data.Full_Data(:,1),Sensing_Data.Full_Data(:,5))
 
 figure
 plot(X1,Y1)
+
+figure
+plot(X2,Y2)
 
 %figure
 %plot(A_Response_Change)
