b32594585a73a5f0040796149578c38efb451028
Sean Hwang <14366381+KingBaba@users.noreply.github.com>
2018-06-01 12:32:21 -0400

    Programmatic fitting incorporated

--- New_Gas_Sensing_Setup_Data_Analysis.m
+++ New_Gas_Sensing_Setup_Data_Analysis.m
@@ -1,6 +1,6 @@
 %Type out file directory
 
-Directory = 'C:\Users\Sean\Box Sync\Graduate School\Research\Data\Sensor\New Gas Sensing';
+Directory = 'C:\Users\seani\Box Sync\Graduate School\Research\Data\Sensor\New Gas Sensing';
 cd(Directory);
 %Fill out information below.
 
@@ -45,18 +45,55 @@ Raw_SourceMeter_Data =  importdata('2018-05-10 - P3-TiO2 Acetone and Humidity Se
 Chip_Count = size(Chip_ID, 2);
 Devices_Count = size(Raw_SourceMeter_Data.data,2)-1;
 
-NSensing_Data.Device_ID = Raw_SourceMeter_Data.textdata(1,:);
+Sensing_Data.Device_ID = Raw_SourceMeter_Data.textdata(1,:);
 Sensing_Data.Full_Data = Raw_SourceMeter_Data.data;
 Sensing_Data.Concentrations = Exposure_Concentrations;
 
 for count1 = 1:Exposures
     
     Field_Variable = compose("Exposure%d", count1);
-    Exposure_Start = fix((Exposure_Time_Points(count1)+Background_Collection_Period)*60/DAQ_Interval+3);
+    Exposure_Start = fix((Exposure_Time_Points(count1)+Background_Collection_Period)*60/DAQ_Interval+3);    
     Purge_End = fix(Exposure_Start + (Exposure_Period + Purge_Period)*60/DAQ_Interval);
-    Sensing_Data.(Field_Variable) = Sensing_Data.Full_Data(Exposure_Start:Purge_End,:);
-    Sensing_Data.(Field_Variable)(:,1) = Sensing_Data.(Field_Variable)(:,1)-Sensing_Data.(Field_Variable)(1,1);
     
+    %Sets point of exposure to t = 0 sec; normalizes current to relative
+    %change from t = 0
+    Data_Normalization = Sensing_Data.Full_Data(Exposure_Start:Purge_End,:);
+    Sensing_Data.(Field_Variable).Normalized_Data = Data_Normalization;
+    Sensing_Data.(Field_Variable).Normalized_Data(:,1) = Data_Normalization(:,1)-Data_Normalization(1,1);
+    Sensing_Data.(Field_Variable).Normalized_Data(:,2:end) = (Data_Normalization(:,2:end)-Data_Normalization(1,2:end))./Data_Normalization(1,2:end);
+    
+    %Fits exposure and response to separate biexponentials.
+    Exposure_End_Index = Exposure_Period*60/DAQ_Interval;
+    
+    Normalized_Response = Data_Normalization(1:Exposure_End_Index,:)-Data_Normalization(1,:);
+    Normalized_Response = Normalized_Response(:,2:end)./Data_Normalization(1,2:end);
+    
+    Recovery_Start_Index = Exposure_End_Index + 1;
+    
+    Normalized_Recovery = Data_Normalization(Recovery_Start_Index:end,:)-Data_Normalization(Recovery_Start_Index,:);
+    Normalized_Recovery = Normalized_Recovery(:,2:end)./Data_Normalization(Recovery_Start_Index,2:end);
+    
+    Sensing_Data.(Field_Variable).Fitting_Data = cell(3,Devices_Count);
+    
+    for count2 = 1:Devices_Count
+        try
+            
+            [Response_Fit, Goodness_of_Fit, Algo_Info] = fit(Normalized_Response(:,1),Normalized_Response(:,count2+1),'exp2');
+            
+        catch
+            
+            warning('Data for a broken device is attempting to be processed');
+            Response_Fit = 0;
+            Goodness_of_Fit = 0;
+            Algo_Info = 0;
+            
+        end
+            
+        Sensing_Data.(Field_Variable).Fitting_Data{1,count2} = Response_Fit;
+        Sensing_Data.(Field_Variable).Fitting_Data{2,count2} = Goodness_of_Fit;
+        Sensing_Data.(Field_Variable).Fitting_Data{3,count2} = Algo_Info;
+        
+    end    
 end
 
 %Data_Points = size(Experimental_Data.data,1);
@@ -72,28 +109,30 @@ end
 %A_Recovery_Change = Experimental_Data.data(Recovery_Start:Recovery_End,2)-Experimental_Data.data(Response_Start,2);
 %A_Recovery_Time = (Experimental_Data.data(Recovery_Start:Recovery_End,1)-Experimental_Data.data(Recovery_Start,1))/1000+1;
 
-X = Sensing_Data.Exposure7(:,1);
-Y = (Sensing_Data.Exposure7(:,5)-Sensing_Data.Exposure7(1,5))/Sensing_Data.Exposure7(1,5);
-X1 = X(1:300);
-Y1 = Y(1:300);
+Chip1_Dev_A_X = Sensing_Data.Exposure7.Normalized_Data(:,1);
+Chip1_Dev_A_Y = Sensing_Data.Exposure7.Normalized_Data(:,2);
+X1 = Chip1_Dev_A_X(1:100);
+Y1 = Chip1_Dev_A_Y(1:100);
 
-X2 = X(301:901);
-Y2 = Y(301:901);
+Chip2_Dev_A_X = Sensing_Data.Exposure7.Normalized_Data(:,1);
+Chip2_Dev_A_Y = Sensing_Data.Exposure7.Normalized_Data(:,8);
+X2 = Chip2_Dev_A_X(1:100);
+Y2 = Chip2_Dev_A_Y(1:100);
 
-X3 = Sensing_Data.Full_Data(305:605,1)-Sensing_Data.Full_Data(305,1);
-Y3 = (Sensing_Data.Full_Data(305:605,5)-Sensing_Data.Full_Data(305,5))/Sensing_Data.Full_Data(305,5);
-
-figure
-plot(X3,Y3)
-
-figure
-plot(Sensing_Data.Full_Data(:,1),Sensing_Data.Full_Data(:,5))
+X3 = Sensing_Data.Full_Data(:,1)-Sensing_Data.Full_Data(1,1);
+Y3_1 = (Sensing_Data.Full_Data(:,2)-Sensing_Data.Full_Data(1,2))/Sensing_Data.Full_Data(1,2);
+Y3_2 = (Sensing_Data.Full_Data(:,8)-Sensing_Data.Full_Data(1,8))/Sensing_Data.Full_Data(1,8);
 
 figure
-plot(X1,Y1)
+plot(X3,Y3_1,X3,Y3_2)
 
+[fit1, gof, output] = fit(X1,Y1,'exp2');
 figure
-plot(X2,Y2)
+plot(fit1,X1,Y1)
+
+fit2 = fit(X2,Y2,'exp2');
+figure
+plot(fit2,X2,Y2)
 
 %figure
 %plot(A_Response_Change)
